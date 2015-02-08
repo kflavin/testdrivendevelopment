ed181f4894b52d99ff5bb36a5d60be1fa3ef9e13
Kyle <Kyle.Flavin@gmail.com>
2015-02-08 12:45:24 -0800

    Adding new settings file for production

--- tdd/tdd/settings/base.py
+++ tdd/tdd/settings/base.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'tdd.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../database/db.sqlite3'),
     }
 }
 
@@ -80,4 +80,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../../static')
+
