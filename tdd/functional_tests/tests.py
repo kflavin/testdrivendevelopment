141377aaa3fbbe7cbd33f5caf9a06d04ef7a6451
Kyle <Kyle.Flavin@gmail.com>
2015-02-07 21:04:14 -0800

    Hack FT runner to be able to test staging

--- tdd/functional_tests/tests.py
+++ tdd/functional_tests/tests.py
@@ -2,10 +2,27 @@ from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
 import time
+import sys
 #from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            print "arg", arg
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super(NewVisitorTest, cls).setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super(NewVisitorTest, cls).tearDownClass()
+
     def setUp(self):
         self.browser = webdriver.Firefox()
     def tearDown(self):
@@ -13,7 +30,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith goes to the homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
@@ -41,7 +58,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User goes to checkout homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         #self.browser.get('http://localhost:8000')
 
         # User notices the page title and header mention to-do lists
@@ -84,7 +101,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Francis vists the home page.  There is no sign of Edith's
         # list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
--- tdd/tdd/settings/base.py
+++ tdd/tdd/settings/base.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-print "Your BASE_DIR is", BASE_DIR
 
 
 # Quick-start development settings - unsuitable for production
