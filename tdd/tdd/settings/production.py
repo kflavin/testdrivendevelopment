d5681f903622ca2594467549cdc949a049f6bd24
Kyle <Kyle.Flavin@gmail.com>
2015-02-08 12:47:31 -0800

    Fixing settings files.

--- tdd/tdd/settings/production.py
+++ tdd/tdd/settings/production.py
@@ -1,3 +1,12 @@
 # Import from base.py, then override individual settings.
 from .base import *
-#DEBUG=False
+
+
+# Production values differ
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, '../../database/db.sqlite3'),
+     }
+}
+STATIC_ROOT = os.path.join(BASE_DIR, '../../static')
